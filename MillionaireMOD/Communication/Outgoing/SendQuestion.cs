acadb493eb825f661863990c960887c09feeaf4d
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-06-13 06:50:35 +0200

    send lifelines

--- MillionaireMOD/Communication/Outgoing/SendQuestion.cs
+++ MillionaireMOD/Communication/Outgoing/SendQuestion.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using HarmonyLib;
+using UnityEngine;
 
 namespace MillionaireMOD.Communication.Outgoing;
 
@@ -16,6 +17,20 @@ internal static class SendQuestion
         if (_lastQuestion == __instance.mQuestion.text) return;
         _lastQuestion = __instance.mQuestion.text;
 
+        List<string> lifelines = [];
+        AnswerStep answer = GameObject.FindObjectOfType<AnswerStep>();
+        if (!answer)
+        {
+            LOGGER.LogError("Uh oh! No AnswerStep");
+        }
+        else
+        {
+            if (!answer.mUIController.mLifelines[0].mUsed.enabled) lifelines.Add("50_50");
+            if (!answer.mUIController.mLifelines[1].mUsed.enabled) lifelines.Add("phone_a_friend");
+            if (!answer.mUIController.mLifelines[2].mUsed.enabled) lifelines.Add("ask_the_audience");
+            if (!answer.mUIController.mLifelines[3].mUsed.enabled) lifelines.Add("flip_the_question");
+        }
+
         Dictionary<string, object> data = new()
         {
             {"question", __instance.mQuestion.text},
@@ -23,6 +38,7 @@ internal static class SendQuestion
             {"answerB", __instance.mAnswers[1].mAnswer.text},
             {"answerC", __instance.mAnswers[2].mAnswer.text},
             {"answerD", __instance.mAnswers[3].mAnswer.text},
+            {"lifelines", lifelines}
         };
 
         WebSocketConnection.Send(new WsMessage("question", data));
