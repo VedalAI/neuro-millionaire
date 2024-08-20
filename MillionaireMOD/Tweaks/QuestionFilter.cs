54dbc53138be2bdf1197a01f5fc8742529058c9b
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-08-20 21:04:43 +0300

    nobody expects the spanish inquisition

--- MillionaireMOD/Tweaks/QuestionFilter.cs
+++ MillionaireMOD/Tweaks/QuestionFilter.cs
@@ -7,13 +7,13 @@ namespace MillionaireMOD.Tweaks;
 [HarmonyPatch]
 internal static class QuestionFilter
 {
-    private static readonly HashSet<int> _blockedQuestions = [];
+    private static readonly HashSet<int> _blockedQuestions = [ 37081, 400789 ];
 
     [HarmonyPatch(typeof(QuestionsLibrary), nameof(QuestionsLibrary.GenerateChildQuestionsPool))]
     [HarmonyPatch(typeof(QuestionsLibrary), nameof(QuestionsLibrary.GenerateQuestionsPool))]
     [HarmonyPatch(typeof(QuestionsLibrary), nameof(QuestionsLibrary.GenerateQuestionsPoolOnline))]
     [HarmonyPostfix]
-    public static void ChildQuestions(QuestionsLibrary __instance, ref int[] __result)
+    public static void FilterQuestions(QuestionsLibrary __instance, ref int[] __result)
     {
         __result = __result.Where(i => !_blockedQuestions.Contains(i)).ToArray();
     }
